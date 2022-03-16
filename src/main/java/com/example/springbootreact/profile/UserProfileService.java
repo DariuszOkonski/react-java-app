ad8ae9840838843449e1f71faaf0240ca7790f25
Dariusz Okonski <darek200180@gmail.com>
2022-03-16 14:11:14 +0000

    build post request

--- src/main/java/com/example/springbootreact/profile/UserProfileService.java
+++ src/main/java/com/example/springbootreact/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package com.example.springbootreact.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -17,5 +19,13 @@ public class UserProfileService {
 
     public List<UserProfile> getUserProfiles() {
         return this.userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        // 1. Check if image is not empty
+        // 2. If file is an image
+        // 3. The user exists in our database
+        // 4. Grab some metadata from file if any
+        // 5. update database
     }
 }
