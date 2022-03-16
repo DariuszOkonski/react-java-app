3224e9df24a0fcda40dcfd6643b992778fbf4856
Dariusz Okonski <darek200180@gmail.com>
2022-03-16 13:43:19 +0000

    build fake datastorage

---
+++ src/main/java/com/example/springbootreact/datastore/FakeUserProfileDataStore.java
@@ -0,0 +1,24 @@
+package com.example.springbootreact.datastore;
+
+import com.example.springbootreact.profile.UserProfile;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class FakeUserProfileDataStore {
+
+    private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+
+    static {
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "janet jones", null));
+        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "antonio junior", null));
+    }
+
+    public List<UserProfile> getUserProfiles() {
+        return USER_PROFILES;
+    }
+
+}
